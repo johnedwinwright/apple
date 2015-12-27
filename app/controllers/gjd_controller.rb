d807764408cd5cccbbfe202f3468f3e9acf52eb8
John Wright <JohnedwinWright@gmail.com>
2015-12-26 20:49:08 -0500

    working request

--- app/controllers/gjd_controller.rb
+++ app/controllers/gjd_controller.rb
@@ -4,23 +4,20 @@ class GjdController < ApplicationController
       require "net/http"
       require "uri"
       uri = URI.parse("https://jobs.apple.com/us/requisition/detail.json")
-      
+
       # Full
       http = Net::HTTP.new(uri.host, uri.port)
       http.use_ssl = true
       http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
-      request = http.request(Net::HTTP::Post.new(uri.request_uri))
-      request.body = "requisitionId=#{params[:jobid]}&reqType=REQ&clientOffset=-300"
+      request = Net::HTTP::Post.new(uri.request_uri)request.body = "requisitionId=#{params[:jobid]}&reqType=REQ&clientOffset=-300"
       request["Content-Type"] = "application/x-www-form-urlencoded"
-      # request["Host"] = "jobs.apple.com"
-      # request["Content-Length"] = "52"
-      # response = http.request(request)
+      response = http.request(request)
       # response.body
-      # response.status
-      @response = "test"
+      # # response.status
+      # @response = "test"
       # @response = response.body.length
-      @response = response.header.
+      @response = response.body
     end
   end
 end
