1e427bffd5aebdd9c0514a1a840d289934f5f729
John Wright <JohnedwinWright@gmail.com>
2015-12-28 15:19:38 -0500

    chuya

--- app/controllers/gji_controller.rb
+++ app/controllers/gji_controller.rb
@@ -32,12 +32,40 @@ class GjiController < ApplicationController
 
       request["Content-Type"] = "application/x-www-form-urlencoded"
       response = http.request(request)
-      # response.body
-      # # response.status
-      # @response = "test"
-      # @response = response.body.length
       @gjiresponse = response.body
       @gjirequest = request.body
+
+      @doc = Nokogiri::XML("#{@gjiresponse}")
+
+      @block = @doc.xpath("//jobId")
+
+      @chld_class = @block.map do |node|
+        node.children.class
+      end
+
+      @chld_class
+      # => [Nokogiri::XML::NodeSet, Nokogiri::XML::NodeSet]
+      # @chld_name = @block.map do |node|
+      #   node.children.map { |n| [n.name] }
+      # end
+
+      @chld_name = @block.map do |node|
+        node.children.map{|n| [n.text]}.compact
+      end.compact
+
+      @chld_name = @chld_name.flatten
+
+      @gjirequest = "#{getjobdetails_path}?jobid=#{@chld_name.first}"
+
+      @chld_name.each do |gjd|
+        require "net/http"
+        require "uri"
+        uri = URI.parse("http://localhost:3000/gjd/getjobdetails?jobid=#{gjd}")
+        http = Net::HTTP.new(uri.host, uri.port)
+        request2 = Net::HTTP::Get.new(uri.request_uri)
+        response2 = http.request(request2)
+         @gjirequest = response.body
+      end
     end
   end
 end
